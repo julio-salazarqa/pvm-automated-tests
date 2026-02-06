8588c108517e349a80db6f33e3030b397ef095a9
julio-salazarqa <julio.salazar@example.com>
2026-02-05 23:16:17 -0600

    Add both test suites to Allure report - Enterprise + Clockwise

--- generate-report.ps1
+++
@@ -1,4 +0,0 @@
-$env:JAVA_HOME = "C:\Program Files\Eclipse Adoptium\jdk-17.0.18.8-hotspot"
-$env:Path = "$env:JAVA_HOME\bin;" + $env:Path
-Set-Location "C:\Users\julio.salazar\Test"
-npx allure generate allure-results --clean -o allure-report
